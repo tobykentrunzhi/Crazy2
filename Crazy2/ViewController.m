123698bfcc78445a7ad21b1e4b4f069b9d25def9
tobykentrunzhi <396367738@qq.com>
2015-10-26 14:20:00 +0800

    /主要是UItoolbar 的items以及 textview 的inputAccessoryView

--- Crazy2/ViewController.m
+++ Crazy2/ViewController.m
@@ -17,11 +17,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIToolbar *topView=[[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 30)];
+    [topView setBarStyle:UIBarStyleDefault];
+    
+    
+    UIBarButtonItem *myBn=[[UIBarButtonItem alloc]initWithTitle:@"无动作" style:UIBarButtonItemStylePlain target:self action:nil];
+    
+    
+    UIBarButtonItem * spaceBn=[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    
+    UIBarButtonItem *doneBn=[[UIBarButtonItem alloc]initWithTitle:@"完成" style:UIBarButtonItemStyleDone target:self action:@selector(editFinish)];
+    
+    NSArray *buttonArrays=@[myBn,spaceBn,doneBn];
+    
+    
+    //items
+    
+    topView.items=buttonArrays;
+    
+    
+    //附件设置view
+    self.textView.inputAccessoryView=topView;
+    
+    
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)editFinish{
+    [self.textView resignFirstResponder];
+    
 }
 
 @end
